6350ba8678be6467a75b131d272952b8bf71d4fb
Matt Ellis <m.t.ellis@gmail.com>
2014-02-12 12:45:25 +0000

    Removed dotTrace profiling API
    Consumed by ReSharper, not exposed

--- src/resharper-apitracker/ReSharperApiApprover.cs
+++ src/resharper-apitracker/ReSharperApiApprover.cs
@@ -11,8 +11,9 @@ namespace CitizenMatt.ReSharper.ApiTracker
     public class ReSharperApiApprover
     {
         private const string ReSharperPath80 = @"C:\work\sdk\v8.0\bin";
+        private const string ReSharperPath81 = @"C:\work\sdk\JetBrains.ReSharper.SDK.8.1.555\bin";
 
-        [TheoryWithLimitedFailures(30)]
+        [TheoryWithLimitedFailures(10)]
         [PropertyData("ReSharper80AssemblyPaths")]
         [UseReporter(typeof(DiffReporter))]
         public void Approve_resharper_80(string assembly, string path)
@@ -20,11 +21,40 @@ namespace CitizenMatt.ReSharper.ApiTracker
             PublicApiApprover.ApprovePublicApi(Path.Combine(path, assembly), @"..\..\results\rs80");
         }
 
+        [TheoryWithLimitedFailures(10)]
+        [PropertyData("ReSharper81AssemblyPaths")]
+        [UseReporter(typeof(DiffReporter))]
+        public void Approve_resharper_81(string assembly, string path)
+        {
+            PublicApiApprover.ApprovePublicApi(Path.Combine(path, assembly), @"..\..\results\rs81");
+        }
+
+        private static readonly ISet<string> IgnoredAssemblies = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            // Consumed by ReSharper, not exposed
+            "JetBrains.dotTrace.ProfilingApi.dll",
+
+            // Test assemblies
+            "JetBrains.ReSharper.Feature.Services.Tests.Common.dll",
+            "JetBrains.ReSharper.Feature.Services.Tests.Basic.dll",
+            "JetBrains.ReSharper.PsiTestsBasic.dll",
+            "JetBrains.ReSharper.TestResources.dll",
+        };
+
         public static IEnumerable<object[]> ReSharper80AssemblyPaths
         {
             get
             {
                 return GetReSharperAssemblies(ReSharperPath80);
+                //yield return new object[] { "JetBrains.ReSharper.Features.Architecture.yFiles.dll", ReSharperPath80 };
+            }
+        }
+
+        public static IEnumerable<object[]> ReSharper81AssemblyPaths
+        {
+            get
+            {
+                return GetReSharperAssemblies(ReSharperPath81);
                 //yield return new object[] { "JetBrains.ReSharper.Features.Architecture.yFiles.dll", ReSharperPath80 };
             }
         }
@@ -42,11 +72,8 @@ namespace CitizenMatt.ReSharper.ApiTracker
         private static bool ShouldInclude(string assembly)
         {
             return assembly.StartsWith("JetBrains", StringComparison.OrdinalIgnoreCase) &&
+                   !IgnoredAssemblies.Contains(assembly) &&
                    !assembly.EndsWith("Tests.dll", StringComparison.OrdinalIgnoreCase) &&
-                   !assembly.Equals("JetBrains.ReSharper.Feature.Services.Tests.Common.dll", StringComparison.OrdinalIgnoreCase) &&
-                   !assembly.Equals("JetBrains.ReSharper.Feature.Services.Tests.Basic.dll", StringComparison.OrdinalIgnoreCase) &&
-                   !assembly.Equals("JetBrains.ReSharper.PsiTestsBasic.dll", StringComparison.OrdinalIgnoreCase) &&
-                   !assembly.Equals("JetBrains.ReSharper.TestResources.dll", StringComparison.OrdinalIgnoreCase) &&
                    !assembly.StartsWith("JetBrains.ReSharper.Refactorings.Test.", StringComparison.OrdinalIgnoreCase) &&
                    !assembly.EndsWith("Tests.Basic.dll", StringComparison.OrdinalIgnoreCase);
         }
